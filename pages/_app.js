cb7bd0c514a97914263e15440fd178e2b486fe26
xeusteerapat <xeus085@gmail.com>
2021-02-08 22:53:20 +0700

    [ADD] add global style

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,58 @@
-import '../styles/globals.css'
+import {
+  ChakraProvider,
+  ColorModeProvider,
+  useColorMode,
+} from '@chakra-ui/react';
+import customTheme from '../styles/theme';
+import { Global, css } from '@emotion/react';
+
+/* ${colorMode === 'light' ? prismLightTheme : prismDarkTheme}; */
+const GlobalStyle = ({ children }) => {
+  const { colorMode } = useColorMode();
+  return (
+    <>
+      <Global
+        styles={css`
+          ::selection {
+            background-color: #90cdf4;
+            color: #fefefe;
+          }
+          ::-moz-selection {
+            background: #ffb7b7;
+            color: #fefefe;
+          }
+          html {
+            min-width: 356px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+            background: ${colorMode === 'light' ? 'white' : '#171717'};
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <ColorModeProvider
+        options={{
+          initialColorMode: 'light',
+          useSystemColorMode: true,
+        }}
+      >
+        <GlobalStyle>
+          <Component {...pageProps} />
+        </GlobalStyle>
+      </ColorModeProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
